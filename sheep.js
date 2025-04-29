46e02db2b3a2d8ed956c0608f73d8e190bb3ecf2
Minkyung Cho <112492904+miincho@users.noreply.github.com>
2025-04-28 22:25:16 -0400

    sheep

--- sheep.js
+++ sheep.js
@@ -1,3 +1,15 @@
+let sheep_left_walk;
+let sheep_left;
+let sheep_right_walk;
+let sheep_right;
+
+function preload() {
+  sheep_left_walk = loadImage('/assets/sheep_left_walk_tiny.gif');
+  sheep_left = loadImage('/assets/sheep_left_tiny.gif');
+  sheep_right_walk = loadImage('/assets/sheep_right_walk_tiny.gif');
+  sheep_right = loadImage('/assets/sheep_right_tiny.gif');
+}
+
 class Sheep {
   constructor(x, y) {
     this.x = x;
@@ -42,7 +54,7 @@ class Sheep {
   }
 
   draw() {
-    ellipse(this.x, this.y, 20, 20);
+    ellipse(this.x, this.y, 1, 1);
 
     // if angle is between 0 and PI --> left
     // if this.isResting, img is resting_left
@@ -51,6 +63,23 @@ class Sheep {
     // else --> right
     // if this.isResting, img is resting_right
     // else img is walking_right
+
+
+    let facingLeft = this.angle >=0 && this.angle < PI;
+
+    if(facingLeft){
+      if(this.isResting){
+        image(sheep_left, this.x -25, this.y -25)
+      }else{
+        image(sheep_left_walk, this.x -25, this.y -25)
+      }
+    }else{
+      if(this.isResting){
+        image(sheep_right, this.x -25, this.y -25);
+      }else{
+        image(sheep_right_walk, this.x -25, this.y-25);
+      }
+    }
   }
 
   startRest() {
